ac30fa32fd6602cab8481776d5cb495d41b57a1c
Tomas Jindra <tomasjindra.iphone@gmail.com>
2022-12-13 23:29:04 +0100

    new implementation of input URLs

--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,7 @@ router.addDefaultHandler(async ({ page, log, enqueueLinks }) => {
         await page.locator('a:has-text("Show more matches")').click()
 
         //TODO - this can be improved - last loop need to wait a bit for Show more button to not be in state visible
-            await page.waitForTimeout(2000)
+        await page.waitForTimeout(2000)
     }
 
     let matchURLs = []
@@ -48,14 +48,14 @@ router.addHandler('match', async ({ request, page, log }) => {
         league: await page.locator("span.tournamentHeader__country a")?.textContent() || null,
         matchTime: await page.locator(".duelParticipant__startTime")?.textContent() || null,
         matchState: await page.locator(".fixedHeaderDuel__detailStatus")?.first().textContent() || "Not started",
-        homeTeamName: await page.locator(".duelParticipant__home")?.textContent() || null,
-        awayTeamName: await page.locator(".duelParticipant__away")?.textContent() || null,
+        homeTeamName: await page.locator(".duelParticipant__home a.participant__participantName")?.textContent() || null,
+        awayTeamName: await page.locator(".duelParticipant__away a.participant__participantName")?.textContent() || null,
         homeTeamScore: await page.locator("div.detailScore__matchInfo div.detailScore__wrapper span")?.first().textContent() || null,
         awayTeamScore: await page.locator("div.detailScore__matchInfo div.detailScore__wrapper span")?.last().textContent() || null,
 
     }
     //if (request.loadedUrl.includes("statistics")) {
-    if (await page.locator('a:has-text("Stats")').isVisible({timeout:2000}) == true) {
+    if (await page.locator('a:has-text("Stats")').isVisible({ timeout: 2000 }) == true) {
         await page.locator('a:has-text("Stats")').click()
         await page.waitForSelector(".stat__category")
 
@@ -66,17 +66,12 @@ router.addHandler('match', async ({ request, page, log }) => {
         const statistics = []
         //TODO is it save to collect the stats like this?
         for (let index = 0; index < statCategories.length; index++) {
-            statistics.push(
-                {
-                    [`${await statCategories[index].textContent()}`]:
-                    {
-                        homeTeamValue: await homeTeamStatValues[index].textContent(),
-                        awayTeamValue: await awayTeamStatValues[index].textContent()
-                    }
-                }
-            )
+            results[`${await statCategories[index].textContent()}`] =
+            {
+                homeTeamValue: await homeTeamStatValues[index].textContent(),
+                awayTeamValue: await awayTeamStatValues[index].textContent()
+            }
         }
-        results = { ...results, statistics }
     }
 
     await Actor.pushData(results)
---
+++ urls.js
@@ -0,0 +1,40 @@
+let object = {
+    
+    basketTeam: "https://www.livesport.com/en/team/crvena-zvezda-mts/6uAuZ6Pt/results/",
+    Original: "https://www.livesport.com/en/team/crvena-zvezda-mts/6uAuZ6Pt/",
+    
+    hockeyTeam: "https://www.livesport.com/en/team/minnesota-wild/j9Ck7BHi/results/",
+    Original: "https://www.livesport.com/en/team/minnesota-wild/j9Ck7BHi/",
+    
+    soccerTeam: "https://www.livesport.com/en/team/manchester-united/ppjDR086/results/",
+    Original: "https://www.livesport.com/en/team/manchester-united/ppjDR086/",
+
+    baseballTeam: "https://www.livesport.com/en/team/toros-del-este/x6rfWKL3/results/",
+    Original: "https://www.livesport.com/en/team/toros-del-este/x6rfWKL3/",
+
+
+    tennisPlayer: "https://www.livesport.com/en/player/nadal-rafael/xUwlUnRK/results/",
+    Original: "https://www.livesport.com/en/player/nadal-rafael/xUwlUnRK/",
+
+//RESENI: pokud je struktura takovahle a chybi tam slovo results tak za tuhle strukturu dat results a pridat do queue
+  //  "https://www.livesport.com/en/team/*/*/
+  // nebo "https://www.livesport.com/en/player/*/*/
+
+  
+    soccerLeague: "https://www.livesport.com/en/soccer/england/premier-league/results/",
+    originalLeague: "https://www.livesport.com/en/soccer/england/premier-league/#/nunhS7Vn/table/overall",
+    
+    basketLeague: "https://www.livesport.com/en/basketball/serbia/superleague/results/",
+    originalLeague: "https://www.livesport.com/en/basketball/serbia/superleague/#/r1Zz4PBS/draw",
+    
+    baseballLeague: "https://www.livesport.com/en/baseball/dominican-republic/lidom/results/",
+    originalLeague: "https://www.livesport.com/en/baseball/dominican-republic/lidom/#/OpNglqVa/table/overall",
+
+
+    hockeyLeague: "https://www.livesport.com/en/nhl/results/",
+    originalLeague: "https://www.livesport.com/en/nhl/#/jkdXuB14/table/overall",
+   
+    
+    tennisLeague: "https://www.livesport.com/en/tennis/atp-singles/australian-open/results/",
+    originalLeague: "https://www.livesport.com/en/tennis/atp-singles/australian-open/#/8Q8MEVel/draw",
+}
